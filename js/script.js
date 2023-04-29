3e52a6bb78229fa277dc78d2b0358204070a78ac
ArtPlatosha <zipex98@gmail.com>
2023-04-29 11:20:45 +0300

    commit: 20th commit
    adding scrolline effect (JS)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
 $(document).ready(function() {
 
-	$(window).on('scroll laod', function() {
-		
-		if($(window).scrollTop() > 60) {
-			$('.header').addClass('active');
-		}
-		else {
-			$('.header').removeClass('active');
-		}
+    $('#menu').click(function() {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('active');
+    });
 
-	})
+    $(window).on('scroll load', function() {
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('active');
+
+        if($(window).scrollTop() > 60) {
+            $('.header').addClass('active');
+        }
+        else {
+            $('.header').removeClass('active');
+        }
+
+        $('section').each(function() {
+
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+
+            if(top >= offset && top < offset + height) {
+                $('.navbar a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+        });
+
+    });
 
 })
